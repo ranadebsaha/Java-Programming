bba47f1ef4239693ff33f2086c3b90a2357be1ce
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-02-15 17:59:43 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    String name;
+    int roll_no;
+
+    void name_roll(String a, int b) {
+        name = a;
+        roll_no = b;
+    }
+
+    void printdata() {
+        System.out.println("Name : " + name);
+        System.out.println("Roll no : " + roll_no);
+    }
+}
+
+class newst {
+    public static void main(String[] rds) {
+        Student s = new Student();
+        s.name_roll("Rupesh", 333);
+        s.printdata();
+    }
+}
