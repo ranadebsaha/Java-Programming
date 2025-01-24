72d3c42efba1c75f1e6358de49b356290649be4a
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2025-01-24 12:29:21 +0530

    Add files via upload

---
+++ arl.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class arl {
+    public static void main(String[] args) {
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("Ranadeb");
+    arr.add("Saha");
+    
+    for(String i:arr){
+        System.out.println(i);
+    }
+}
+}
